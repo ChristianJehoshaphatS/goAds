ccbc9fd0308edee0e53cbf52206d02435f4837ce
AisyahAuliaAngelinee <vincentius.clarishna@gmail.com>
2023-09-28 12:41:40 +0700

    commit vincent

---
+++ js/stylescript.js
@@ -0,0 +1,18 @@
+// gototop reaction
+window.addEventListener('scroll', function () {
+    var scrollToTopButton = document.getElementById('scrollToTopButton');
+    if (window.pageYOffset > 300) {
+        scrollToTopButton.classList.add('show');
+    } else {
+        scrollToTopButton.classList.remove('show');
+    }
+});
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
+
+document.getElementById('scrollToTopButton').addEventListener('click', scrollToTop);
